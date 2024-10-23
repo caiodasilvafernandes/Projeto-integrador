d3eed04913ba0b1d70644c8a86dca3f3f49cc224
Caio Fernandes <dasilvafernandescaio@gmail.com>
2024-10-23 10:51:28 -0300

    Correção da função de favoritar

--- control/controleXML.js
+++ control/controleXML.js
@@ -7,7 +7,8 @@ router.post("/ControleFavorita/:idPack/:fav", manipulaToken.verificaToken, (req,
     let { fav, idPack } = req.params;
     let idCliente = req.userId;
     idPack = parseInt(idPack)
-
+    
+    
     if (fav == 1) {
         let queryFav = "INSERT INTO pacotesFav_Comp (idFKPacote,idFkCliente,tipo) VALUES (?,?,?);";
 
@@ -16,7 +17,6 @@ router.post("/ControleFavorita/:idPack/:fav", manipulaToken.verificaToken, (req,
         } catch (err) {
             console.log(err);
         }
-        return;
     }
     else {
         let queryDeleteFav = "DELETE FROM pacotesFav_Comp WHERE idFkPacote = ? AND idFkCliente = ? AND tipo = ?;";
@@ -26,6 +26,30 @@ router.post("/ControleFavorita/:idPack/:fav", manipulaToken.verificaToken, (req,
         } catch (err) {
             console.log(err);
         }
+    }
+});
+
+router.post("/ControleAvalia/:idPack/:nota", async (req, res) => {
+    let { idPack, nota } = req.params;
+    let idCliente = req.userId;
+    let queryVerifica = "SELECT idPacoteFav,cliente.idCliente,idPacote INNER JOIN cliente ON cliente.idCliente = pacotesFav_Comp.idCliente FROM pacotesFav_Comp;";
+    let insertAvalia = "INSERT INTO pacotesFav_Comp(avaliacao,idPacote,idCliente) VALUES (?,?,?);";
+    let updateAvalia = "UPDATE avaliacao SET avaliacao = ? WHERE idPacote = ?   AND idCliente = ?;"
+
+    try {
+        var verificaAvalia = await new Promise((resolve, reject) => {
+            conn.query(queryVerifica, (err, result) => {
+                resolve(result);
+            });
+        });
+    } catch (err) {
+        console.log(err);
+    }
+
+    if (!verificaAvalia) {
+        conn.query(insertAvalia, [nota, idPack, idCliente]);
+    }else{
+        conn.query(updateAvalia, [nota, idPack, idCliente])
     }
 });
 
--- public/js/kitPageAjax.js
+++ public/js/kitPageAjax.js
@@ -2,10 +2,13 @@ const xhttp = new XMLHttpRequest();
 
 function controleFav(method, id, fav) {
     id = parseInt(id);
+    
     xhttp.open(method, `/ControleFavorita/${id}/${fav}`);
     xhttp.send();
 }
 
-function controleAvalia(method,id){
-    
+function controleAvalia(method, id, nota) {
+    id = parseInt(id);
+    xhttp.open(method, `/ControleAvalia/${id}/${nota}`);
+    xhttp.send();
 }
